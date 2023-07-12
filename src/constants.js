8c103f79df23fa9051e865b8584176f34d8d6033
Hiren Soni <65155846+hirensoni913@users.noreply.github.com>
2023-07-12 11:09:23 +0300

    OP-1473: Payment status lists multiple rejected options (#18)
    * Payment status bug fixed
    
    * Corrected a typo

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,10 @@
-export const RIGHT_PAYMENT = 101401
-export const RIGHT_PAYMENT_SEARCH = 101401
-export const RIGHT_PAYMENT_ADD = 101402
-export const RIGHT_PAYMENT_EDIT = 101403
-export const RIGHT_PAYMENT_DELETE = 101404
+export const RIGHT_PAYMENT = 101401;
+export const RIGHT_PAYMENT_SEARCH = 101401;
+export const RIGHT_PAYMENT_ADD = 101402;
+export const RIGHT_PAYMENT_EDIT = 101403;
+export const RIGHT_PAYMENT_DELETE = 101404;
 
 export const MODULE_NAME = "payment";
 
-export const PAYMENT_STATUS = [-3, -2, -1, 1, 2, 3, 4, 5];
-export const PAYMENTS_TAB_VALUE = "paymentsTab"
+export const PAYMENT_STATUS = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
+export const PAYMENTS_TAB_VALUE = "paymentsTab";
